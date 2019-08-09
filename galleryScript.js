85e314a3d62c7a1ec9d6dfc48a624d266ed53c7b
V1ncentLiu <vincentliu9816@gmail.com>
2019-08-08 20:07:10 -0400

    Second Update

--- galleryScript.js
+++ galleryScript.js
@@ -4,34 +4,54 @@ var mid = document.getElementById("mid");
 var lft = document.getElementById("lft");
 var rht = document.getElementById("rht");
 
-function pushFile(evt){
-	var files = evt.target.files;
-	for (var i = 0, f; f = files[i]; i++){
-		seq.push()
+function loadPic(){
+	var path = "img/";
+	$.ajax({
+		url: path,
+		success: function(data){
+			$(data).fild("a").attr("href", function(i, val){
+				if(val.match(/\.(jpe?g|png)$/)){
+					$(seq).push(path + val);
+				}
+			});
+		}
+	});
+	/*
+	var temp = path.getFiles();
+	for(var i = 0; i < path.length; i++){
+		var f = temp[i];
+		seq.push(f.name);
 	}
-	
+	*/
 }
-
-
 
 function next(){
 	var i = 0;
-	rht.src = seq[i];
-	mid.src = seq[i - 1];
-	lft.src = seq[i - 2];
-	i++;
-	if(i>=seq.length){
-		i = 0;
+	for(i = 0; i >= seq.length; i++){
+		rht.src = seq[i];
+		mid.src = seq[i - 1];
+		lft.src = seq[i - 2];
+		if(i >= seq.length){
+			i = 0;
+		}
 	}
 }
 
 function prev(){
 	var i = 0;
+	for(i = seq.length; i <= 0; i--)
 	lft.src = seq[i];
 	mid.src = seq[i + 1];
 	rht.src = seq[i + 2];
-	i--;
 	if(i <= seq.length){
 		i = 0;
 	}
+}
+
+function resetPic(){
+	mid.src=seq[0];
+}
+
+function initialize(){
+	loadPic();
 }
